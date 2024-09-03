1823524a9e5068cee4c8a735d29dd8926ab0563f
Jaydeep Godhani <godhani.rameshbhai@walmart.com>
2024-09-03 23:09:29 +0530

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 function App() {
-
   return (
-    <>
-      <p className="text-3xl font-bold p-8">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
 }
 
-export default App
+export default App;
